e131f6693463d6c911f0efbdb06fb3ef00d9a74a
Nebby00000 <patrickluisnebres08@gmail.com>
2024-10-09 13:09:33 +0800

    added a login

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, ContactPageView
+from .views import LoginPageView, HomePageView, AboutPageView, ContactPageView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
-    path('/about', AboutPageView.as_view(), name='about'),
-    path('/contact', ContactPageView.as_view(), name='contact'),
+    path('', LoginPageView, name='login'),
+    path('home/', HomePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('contact/', ContactPageView.as_view(), name='contact'),
+
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from django.views.generic import TemplateView
+
+
+def LoginPageView(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+
+            return render(request,'app/login.html', {'error': 'Invalid username or password'})
+    return render(request, 'app/login.html')
+
+
+
 
 class HomePageView(TemplateView):
     template_name = 'app/home.html'
