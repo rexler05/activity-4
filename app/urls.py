103d4c645a280447d1c09bff97f8c530cd8094e8
rexler05 <esculturarexler@gmail.com>
2024-10-11 18:06:49 +0800

    inserted an image ico

--- app/urls.py
+++ app/urls.py
@@ -3,9 +3,9 @@ from .views import LoginPageView, HomePageView, AboutPageView, ContactPageView
 from .import views
 
 urlpatterns = [
-    path('', LoginPageView, name='login'),
-    path('join', views.join, name='join'),
-    path('home/', HomePageView.as_view(), name='home'),
+    path('login/', LoginPageView, name='login'),
+    path('join/', views.join, name='join'),
+    path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
     path('contact/', ContactPageView.as_view(), name='contact'),
 
