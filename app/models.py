f38d0c886c60f66c541415eb9bed0b2e309a8886
rexler05 <esculturarexler@gmail.com>
2024-10-15 23:42:04 +0800

    updating it again

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Member(models.Model):
     fname = models.CharField(max_length=50)
     lname = models.CharField(max_length=100)
-    email = models.EmailField(max_length=200)
+    email = models.EmailField(max_length=20)
     username = models.CharField(max_length=100, null=True)
     passwd = models.CharField(max_length=50)
     age = models.IntegerField()
